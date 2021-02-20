160761a3f93acc9596f46a616acd87f822951ea9
brandon <zxczoxc125@naver.com>
2021-02-20 15:43:00 +0900

    ✅ redux 기본 세팅

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./components/App";
 
+const middleware = [];
+const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case "TEST":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
 ReactDOM.render(
-   <App />,
-   document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
