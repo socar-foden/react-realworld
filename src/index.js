3e41b5cc735f7f55788654d4a659b95df325c427
brandon <zxczoxc125@naver.com>
2021-02-20 14:51:15 +0900

    🔧 babel 기본 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-document.getElementById("root");
-
-console.log('hello');
+ReactDOM.render(
+   <App />,
+   document.getElementById("root")
+);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,6 +9,20 @@ module.exports = {
     }),
   ],
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
