d84df06c9c6661c2f7f09c10d56403e4b1751b01
brandon <zxczoxc125@naver.com>
2021-02-20 10:41:33 +0900

    🔧 환경설정 분리

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,11 +1,14 @@
-var path = require("path");
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  plugins: [new HtmlWebpackPlugin({
-     template: './index.html'
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  mode: "development",
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
