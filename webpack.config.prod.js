c378f314b6fdff01ea365cb172602c166ea589a6
brandon <zxczoxc125@naver.com>
2021-02-21 10:15:54 +0900

    🔧 prod 바벨 로더 추가

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,19 @@
 module.exports = {
   entry: "./src/index.js",
-  plugins: [
-  ],
+  plugins: [],
   mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
 };
