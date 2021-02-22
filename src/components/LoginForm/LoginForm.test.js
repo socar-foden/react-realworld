764f173f64d4ac8c377b2bfe984c372bafc50122
brandon <zxczoxc125@naver.com>
2021-02-22 10:55:20 +0900

    🔥 불필요 코드 제거

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -5,7 +5,7 @@ import LoginForm from "./LoginForm";
 
 describe("[LoginForm]", () => {
   it("id, password input이 존재", () => {
-    const { container, unmount, getByRole } = render(<LoginForm />);
+    const { unmount, getByRole } = render(<LoginForm />);
     expect(getByRole("input", { name: 'email' })).toBeInTheDocument();
     expect(getByRole("input", { name: 'password' })).toBeInTheDocument();
     unmount();
