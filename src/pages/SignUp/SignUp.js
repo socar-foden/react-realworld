019a5cbebcc0541ceb7dd1b16eee2440c73f8ae4
brandon <zxczoxc125@naver.com>
2021-02-23 14:19:34 +0900

    ✨ 유효하지 않은 폼 요청시 alert

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Alert from "../../components/Alert/Alert";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import { userActions } from "../../reducers/user/userReducer";
 import useStyles from "./SignUp.style";
 
 const SignUp = () => {
   const classes = useStyles();
+  const {
+    registration: {
+      failure: registration_failure,
+    },
+  } = useSelector((rootReducer) => rootReducer.userReducer);
+  const dispatch = useDispatch();
+
+  const init_registration_failure = () => {
+    dispatch(
+      userActions.registration_failure({
+        errors: { body: "" },
+      })
+    );
+  };
+
+  const handleCloseErrorAlert = () => {
+    init_registration_failure();
+  };
+
+  useEffect(() => {
+    init_registration_failure();
+  }, []);
 
   return (
     <div className={classes.root}>
       <SignUpForm />
+
+      {registration_failure && (
+        <Alert
+          message={registration_failure}
+          severity={"error"}
+          handleOnClose={handleCloseErrorAlert}
+        />
+      )}
     </div>
   );
 };
