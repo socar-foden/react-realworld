ed925cf37230718c2f67e19ef6522c597c89d8b5
brandon <zxczoxc125@naver.com>
2021-02-24 22:25:39 +0900

    ⚡️ 형식 수정

--- src/apis/user/user-mock-service.js
+++ src/apis/user/user-mock-service.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 
-const user_mock_service = {};
-
-user_mock_service.registration = ({ user }) => {
-  const mock = new MockAdapter(axios);
-  mock.onPost("/api/users").reply(200, user);
-  return axios.post("/api/users");
+const user_mock_service = {
+  registration: ({ user }) => {
+    const mock = new MockAdapter(axios);
+    mock.onPost("/api/users").reply(200, user);
+    return axios.post("/api/users");
+  },
 };
 
 export default user_mock_service;
