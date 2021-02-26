df0ffd51f716a33226a1f06148e50a171541bb0e
brandon <zxczoxc125@naver.com>
2021-02-26 16:05:16 +0900

    ✨ 실제 service로 변경

--- src/sagas/user/userSaga.js
+++ src/sagas/user/userSaga.js
@@ -1,10 +1,9 @@
 import { all } from "redux-saga/effects";
 import mock_user_service from "../../apis/user/mock_user_service";
+import user_service from "../../apis/user/user_service";
 import { userActions } from "../../reducers/user/userReducer";
 import { watchSaga } from "../../utils/utils";
 
 export default function* userSaga() {
-  yield all([
-    watchSaga(userActions.REGISTRATION, user_mock_service.registration),
-  ]);
+  yield all([watchSaga(userActions.REGISTRATION, user_service.registration)]);
 }
