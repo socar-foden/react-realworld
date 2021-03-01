932721e99fc82cf86d285bbc7d0ccb29355b9bda
brandon <zxczoxc125@naver.com>
2021-03-01 15:53:51 +0900

    🔧 history fall back 설정

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const path = require("path");
 const express = require("express");
-require("dotenv").config({ path: path.join(__dirname, `./.env.${process.env.NODE_ENV}`) });
+const fallback = require("express-history-api-fallback");
+require("dotenv").config({
+  path: path.join(__dirname, `./.env.${process.env.NODE_ENV}`),
+});
 
 const app = express();
+const root = path.join(__dirname, "/dist");
 
-app.use(express.static('dist'));
+app.use(express.static(root));
+app.use(fallback("index.html", { root: root }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./dist/index.html"));
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`✅ Server is running on [ http://localhost:${process.env.PORT} ]`);
+  console.log(
+    `✅ Server is running on [ http://localhost:${process.env.PORT} ]`
+  );
 });
