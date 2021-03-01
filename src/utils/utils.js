13820f7403121e42786dbbd0410c30598fd897c9
brandon <zxczoxc125@naver.com>
2021-03-01 14:14:58 +0900

    ⚡️ createAsyncSaga 수정

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,12 @@
 import fp from "lodash/fp";
-import { fork, put, takeLatest } from "redux-saga/effects";
+import { call, fork, put, takeLatest } from "redux-saga/effects";
 
 const utils = {
   validate: fp.find(({ v, pred }) => !pred(v)),
   createAsyncSaga: (type, api) =>
     function* ({ payload }) {
       try {
-        const res = yield fork(api, payload);
+        const res = yield call(api, payload);
         yield put({ type: `${type}_SUCCESS`, payload: res });
       } catch (e) {
         console.error(`[Saga-${type}] ERROR :: `, e);
