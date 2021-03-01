7c7c365bbdfa20fde09ec77e98f3f4cc5b814c8d
brandon <zxczoxc125@naver.com>
2021-03-01 13:53:30 +0900

    ✨ 인증 api 추가

--- src/apis/user/user_service.js
+++ src/apis/user/user_service.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const user_mock_service = {
   registration: ({ user }) => axios.post("/api/users", { user }),
+  authentication: ({ user }) => axios.post("/api/users/login", { user }),
 };
 
 export default user_mock_service;
