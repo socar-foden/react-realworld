26dc6328ed95b149926e8d9e5e385460a4da97e0
brandon <zxczoxc125@naver.com>
2021-03-01 15:43:30 +0900

    ✨ 회원가입 성공시 /로 이동

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
 import Alert from "../../components/Alert/Alert";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import { userActions } from "../../reducers/user/userReducer";
@@ -9,10 +10,12 @@ const SignUp = () => {
   const classes = useStyles();
   const {
     registration: {
+      success: registration_success,
       failure: registration_failure,
     },
   } = useSelector((rootReducer) => rootReducer.userReducer);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const init_registration_failure = () => {
     dispatch(
@@ -24,6 +27,11 @@ const SignUp = () => {
 
   const handleCloseErrorAlert = () => {
     init_registration_failure();
+  };
+
+  const handleCloseSuccessAlert = () => {
+    init_registration_failure();
+    history.push("/");
   };
 
   useEffect(() => {
@@ -39,6 +47,14 @@ const SignUp = () => {
           message={registration_failure}
           severity={"error"}
           handleOnClose={handleCloseErrorAlert}
+        />
+      )}
+
+      {registration_success && (
+        <Alert
+          message={"You have been signed up. Please log in."}
+          severity={"success"}
+          handleOnClose={handleCloseSuccessAlert}
         />
       )}
     </div>
