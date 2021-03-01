5751c6e8915e64140819deffaf9ebe5d12828b9f
brandon <zxczoxc125@naver.com>
2021-03-01 13:56:08 +0900

    ⚡️ prop 변경

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -3,15 +3,21 @@ import MuiAlert from "@material-ui/lab/Alert";
 import useStyles from "./Alert.style";
 import { Snackbar } from "@material-ui/core";
 
-const Alert = ({ severity = "error", message, handleOnClose }) => {
+const Alert = ({
+  severity = "error",
+  message,
+  handleOnClose,
+  autoHideDuration = 1000,
+}) => {
   const classes = useStyles();
 
   return (
     <Snackbar
       className={classes.root}
-      autoHideDuration={1000}
+      autoHideDuration={autoHideDuration}
       open={true}
       onClose={handleOnClose}
+      on
     >
       <MuiAlert elevation={6} variant="filled" severity={severity}>
         {message}
