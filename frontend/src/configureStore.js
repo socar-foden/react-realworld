2160ad1ce4ab43fd1c0761e20a74ba6698640468
brandon <zxczoxc125@naver.com>
2021-03-05 18:44:42 +0900

    ⚡️ redux-persist 적용

---
+++ frontend/src/configureStore.js
@@ -0,0 +1,30 @@
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./reducers/rootReducer";
+import rootSaga from "./sagas/rootSaga";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware(rootSaga);
+
+  const store = createStore(
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  );
+
+  sagaMiddleware.run(rootSaga);
+  const persistor = persistStore(store);
+  
+  return { store, persistor };
+};
+
+export default configureStore;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,34 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import axios from "axios";
+import { PersistGate } from "redux-persist/integration/react";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 // core-js/stable, regenerator-runtime/runtime를 임포트 한 후에 redux-saga를 임포트해야함
 // https://redux-saga.js.org/
-import createSagaMiddleware from "redux-saga";
 import App from "./components/App";
-import rootReducer from "./reducers/rootReducer";
-import rootSaga from "./sagas/rootSaga";
+import configureStore from "./configureStore";
+
+const { store, persistor } = configureStore();
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "????";
 
-const sagaMiddleware = createSagaMiddleware(rootSaga);
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(sagaMiddleware))
-);
-
-sagaMiddleware.run(rootSaga);
-
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
