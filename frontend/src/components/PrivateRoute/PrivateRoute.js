f77541116a491189cd5c9729427453376fa869a5
brandon <zxczoxc125@naver.com>
2021-03-07 10:58:20 +0900

    ⚡️ 버그 수정

--- frontend/src/components/PrivateRoute/PrivateRoute.js
+++ frontend/src/components/PrivateRoute/PrivateRoute.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import fp from "lodash/fp";
 import { Redirect, Route } from "react-router";
 
 const PrivateRoute = ({ children, ...props }) => {
-  const {
-    user: { token },
-  } = useSelector((rootReducer) => rootReducer.userReducer);
+  const { user } = useSelector((rootReducer) => rootReducer.userReducer);
 
   return (
-    <Route {...props}>{token ? children : <Redirect to="/sign-in" />}</Route>
+    <Route {...props}>{fp.get('token', user) ? children : <Redirect to="/sign-in" />}</Route>
   );
 };
 
