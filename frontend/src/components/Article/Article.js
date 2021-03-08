96db6e483207c9b15abb6e835d87cc2461245d23
brandon <zxczoxc125@naver.com>
2021-03-08 09:42:38 +0900

    ⚡️ 프로필 사진부분 변경

--- frontend/src/components/Article/Article.js
+++ frontend/src/components/Article/Article.js
@@ -3,10 +3,10 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import CardActions from "@material-ui/core/CardActions";
 import Avatar from "@material-ui/core/Avatar";
-import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import FavoriteIcon from "@material-ui/icons/Favorite";
@@ -24,9 +24,13 @@ const Article = () => {
           //  <Avatar aria-label="recipe" className={classes.avatar}>
           //    R
           //  </Avatar>
-          <div>
-            <AccountBoxIcon className={classes.cover} />
-          </div>
+          <Button
+            aria-label="author"
+            color="inherit"
+            // onClick={handleClickSignOutIcon}
+          >
+            <AccountCircleIcon className={classes.cover} />
+          </Button>
         }
         action={
           <IconButton aria-label="settings">
