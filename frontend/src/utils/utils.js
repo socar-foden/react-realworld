6c0aeba4a16050a380b8bea07c334b52eae9c77f
brandon <zxczoxc125@naver.com>
2021-03-10 10:39:48 +0900

    ✨  액션 로그 이모지 추가

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -8,10 +8,10 @@ const utils = {
       try {
         const { data } = yield call(api, payload);
         yield put({ type: `${type}_SUCCESS`, payload: data });
-        console.log(`[Saga - ${type}] :: `, data);
+        console.log(`✅ [Saga - ${type}] :: `, data);
       } catch (e) {
         const { errors } = e.response.data;
-        console.error(`[Saga - ${type}] ERROR :: `, errors);
+        console.error(`❌ [Saga - ${type}] ERROR :: `, errors);
         yield put({
           type: `${type}_FAILURE`,
           payload: {
