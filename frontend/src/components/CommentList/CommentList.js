ea3581280b6b7b5c10bd0c99ab18499193a9599e
brandon <zxczoxc125@naver.com>
2021-03-11 22:52:53 +0900

    ✨ 스타일 추가

--- frontend/src/components/CommentList/CommentList.js
+++ frontend/src/components/CommentList/CommentList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import fp from "lodash/fp";
 import Comment from "../Comment/Comment";
+import useStyles from "./CommentList.style";
 
 const CommentList = ({ comments = [] }) => {
+  const classes = useStyles();
+
   return (
-    <div role="figure" aria-label="comment-list">
+    <div role="figure" aria-label="comment-list" className={classes.root}>
       {fp.map(
         (comment) => (
           <Comment key={comment.id} comment={comment} />
