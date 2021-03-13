ec5696f4a94e122da0687aaebf3f5bd4e1b2bf6a
brandon <zxczoxc125@naver.com>
2021-03-13 13:09:04 +0900

    ✨ add comment 추가

--- frontend/src/apis/comment/comment_service.js
+++ frontend/src/apis/comment/comment_service.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const comment_service = {
   getCommentsFromAnArticle: ({ slug }) =>
     axios.get(`/api/articles/${slug}/comments`),
+  addCommentsToAnArticle: ({ slug, comment }) =>
+    axios.post(`/api/articles/${slug}/comments`, { comment }),
 };
 
 export default comment_service;
