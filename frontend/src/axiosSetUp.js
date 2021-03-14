f22bae98a5032b53b5165b7e9dfe1bdedfb8a4a5
brandon <zxczoxc125@naver.com>
2021-03-14 17:12:00 +0900

    ⚡️ base url 변경

--- frontend/src/axiosSetUp.js
+++ frontend/src/axiosSetUp.js
@@ -5,10 +5,8 @@ export const TOKEN_KEY = "realworld-jwt";
 const axiosSetUp = () => {
   const token = localStorage.getItem(TOKEN_KEY);
 
-  // TODO: 공유기 포트포워딩 해주고
-  // TODO: development URL http://220.70.46.155:3000로 변경
   axios.defaults.baseURL =
-    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "Production URL"
+    process.env.NODE_ENV === "development" ? "http://220.70.46.155:3000" : "http://220.70.46.155:9000"
   if (token) {
     axios.interceptors.request.use((config) => {
       config.headers.Authorization = `Token ${token}`;
