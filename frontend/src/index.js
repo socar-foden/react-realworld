b55eace6d2895b9f188e10d3bca9fd1ec04fbf30
brandon <zxczoxc125@naver.com>
2021-03-15 15:03:30 +0900

    🔥 redux-persist 관련 코드, 의존성 제거

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import createSagaMiddleware from "redux-saga";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 // core-js/stable, regenerator-runtime/runtime를 임포트 한 후에 redux-saga를 임포트해야함
 // https://redux-saga.js.org/
 import App from "./components/App";
-import configureStore from "./configureStore";
 import axiosSetUp from "./axiosSetUp";
+import rootSaga from "./sagas/rootSaga";
+import authentication from "./middlewares/authentication";
+import rootReducer from "./reducers/rootReducer";
 
-const { store, persistor } = configureStore();
 axiosSetUp();
+
+const sagaMiddleware = createSagaMiddleware(rootSaga);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, authentication))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
