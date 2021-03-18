732329967a9ed6f38ee40364da814f1bf577c586
brandon <zxczoxc125@naver.com>
2021-03-18 14:01:26 +0900

    ⚡️ 스타일 수정

--- frontend/src/components/Progress/Progress.style.js
+++ frontend/src/components/Progress/Progress.style.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
-    color: "white",
+    color: "#fff",
   },
 }));
 
